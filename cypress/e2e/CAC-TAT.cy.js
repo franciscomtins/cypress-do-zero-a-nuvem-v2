b6e986be752e343d079ee6cf8d543aa7b7633160
Francisco Martins <francisco.mtins@gmail.com>
2026-03-07 17:56:58 -0300

    Add testes com custom commands

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -6,6 +6,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     nome: faker.person.firstName(),
     sobrenome: faker.person.lastName(),
     email: faker.internet.email(),
+    telefone: faker.string.numeric(10),
     comoPodemosAjudar: faker.lorem.sentences(2)
   }
 
@@ -48,11 +49,90 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .should('be.visible', 'Valide os campos obrigatórios!')
   })
 
-  it.only('Campo telefone so aceita números', () => {
+  it('Campo telefone so aceita números', () => {
     cy.get('#phone')
       .type('xpto')
       .should('be.empty')
       //.should('have.value', '')
+  })
+
+  it('Valida telefone obrigatório mas não é preenchido ao enviar do formulário', () => {
+    cy.get('#firstName').type(contato.nome)
+    cy.get('#lastName').type(contato.sobrenome)
+    cy.get('#email').type(contato.email)  
+    cy.get('#open-text-area').type(contato.comoPodemosAjudar, { delay:0 }) 
+    cy.get('#phone-checkbox').check()
+
+    cy.get('button[type="submit"]')
+      .contains('Enviar')
+      .click()
+
+    cy.get('.error > strong')
+      .should('be.visible',' Valide os campos obrigatórios!')
+  })
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName')
+      .type(contato.nome)
+      .should('have.value', contato.nome)
+      .clear()
+      .should('have.value', '')
+
+    cy.get('#lastName')
+      .type(contato.sobrenome)
+      .should('have.value', contato.sobrenome)
+      .clear()
+      .should('be.empty')
+
+    cy.get('#email')
+      .type(contato.email)
+      .should('have.value', contato.email)
+      .clear()
+      .should('have.value', '')
+    
+    cy.get('#phone')
+      .type(contato.telefone)
+      .should('have.value', contato.telefone)
+      .clear()
+      .should('be.empty')
+  })
+
+  it('Valida submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.get('button[type="submit"]')
+      .contains('Enviar')
+      .click()
+
+    cy.get('.error > strong')
+      .should('be.visible', 'Valide os campos obrigatórios!')
+  })
+
+  // usando comando com dados hard code
+  it('Envia o formuário com sucesso usando um comando customizado', () => {  
+    cy.preencheCamposObrigatoriosEenvia()
+
+    cy.get('.success > strong')
+      .should('be.visible', 'Mensagem enviada com sucesso.')
+  })
+
+  // usando comando e faker
+  it('Envia o formulario com sucesso usando faker e comando customizado', () => {
+
+    cy.preencherFormularioContatoEenvia(contato)
+
+    cy.get('.success > strong')
+      .should('be.visible')
+      .and('contain', 'Mensagem enviada com sucesso.')
+  })
+
+  // usando comando e faker + padrão -> retire a chamada do objeto na função para chamar valores 
+  // definidos como padrão no objeto do custom commands
+  it.only('Envia o formulario com sucesso usando faker + padrao e comando customizado', () => {
+
+    cy.preencherFormularioContatoEenviaPadrao()
+
+    cy.get('.success > strong')
+      .should('be.visible')
+      .and('contain', 'Mensagem enviada com sucesso.')
   })
 
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,43 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// comando com dados hard code
+Cypress.Commands.add('preencheCamposObrigatoriosEenvia', () => {  
+  cy.get('#firstName').type('primeiroNome')
+  cy.get('#lastName').type('ultimoNome')
+  cy.get('#email').type('meuemail@email.com')  
+  cy.get('#open-text-area').type('campo de texto inserido', { delay:0 })   
+
+  cy.get('button[type="submit"]')
+    .contains('Enviar')
+    .click()
+})
+
+// comando com dados gerados pela faker
+Cypress.Commands.add('preencherFormularioContatoEenvia', (contato) => {
+  cy.get('#firstName').type(contato.nome)
+  cy.get('#lastName').type(contato.sobrenome)
+  cy.get('#email').type(contato.email)
+  cy.get('#phone').type(contato.telefone)
+  cy.get('#open-text-area').type(contato.comoPodemosAjudar, { delay: 0 })
+
+  cy.get('button[type="submit"]')
+    .contains('Enviar')
+    .click()
+})
+
+// comando com dados gerados pela faker + padrão 
+Cypress.Commands.add('preencherFormularioContatoEenviaPadrao', (contato = {
+    nome: 'PrimeiroNomePadrão',
+    sobrenome: 'SobrenomePadrão',
+    email: 'emailpadrao@email.com',
+    telefone: 8533221199,
+    comoPodemosAjudar: 'Texto padrão escrito'
+}) => {
+  cy.get('#firstName').type(contato.nome)
+  cy.get('#lastName').type(contato.sobrenome)
+  cy.get('#email').type(contato.email)
+  cy.get('#phone').type(contato.telefone)
+  cy.get('#open-text-area').type(contato.comoPodemosAjudar, { delay: 0 })
+
+  cy.get('button[type="submit"]')
+    .contains('Enviar')
+    .click()
+})
